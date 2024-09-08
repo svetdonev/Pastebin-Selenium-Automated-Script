6396ec5c94dbdccc5b81e08cc52c0dc4ffad1354
Svetlozar Donev <svetdonev1@gmail.com>
2024-09-08 11:27:24 +0300

    Implement Page class

---
+++ Pastebin-Script/PageTests.cs
@@ -0,0 +1,16 @@
+namespace Pastebin_Script
+{
+    public class PageTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Pastebin-Script/Pages/Page.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pastebin_Script.Pages
+{
+    public class Page
+    {
+        private IWebDriver driver;
+        public Page(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public static string PastebinUrl = "https://pastebin.com/";
+        public IWebElement CodeInput => driver.FindElement(By.Id("postform-text"));
+        public IWebElement ExpirationDropdown => driver.FindElement(By.Id("select2-postform-expiration-container"));
+        public IWebElement PasteNameInput => driver.FindElement(By.Id("postform-name"));
+        public IWebElement CreateNewPasteButton => driver.FindElement(By.XPath("//button[text()='Create New Paste']"));
+        public void SelectPasteExpiration(string expirationOption)
+        {
+            ExpirationDropdown.Click();
+            driver.FindElement(By.XPath($"//li[text()='{expirationOption}']")).Click();
+        }
+        public void CreateNewPaste(string code, string expirationOption, string name)
+        {
+            CodeInput.SendKeys(code);
+            SelectPasteExpiration(expirationOption);
+            PasteNameInput.SendKeys(name);
+            CreateNewPasteButton.Click();
+        }
+    }
+}
